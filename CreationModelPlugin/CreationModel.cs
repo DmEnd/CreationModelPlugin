0d689d9c8936ca30bd590236ab512c3cfd8f9c92
DmEnd <Dima_polit-sov@mail.ru>
2023-02-21 00:44:37 +0300

    Добавлены методы построения стен и окон.

--- CreationModelPlugin/CreationModel.cs
+++ CreationModelPlugin/CreationModel.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,8 @@ namespace CreationModelPlugin
     [TransactionAttribute(TransactionMode.Manual)]
     public class CreationModel : IExternalCommand
     {
+        List<Wall> walls = new List<Wall>();
+
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             Document doc = commandData.Application.ActiveUIDocument.Document;
@@ -32,9 +35,62 @@ namespace CreationModelPlugin
             double width = UnitUtils.ConvertToInternalUnits(10000, UnitTypeId.Millimeters);
             double depth = UnitUtils.ConvertToInternalUnits(5000, UnitTypeId.Millimeters);
 
-            CreateWalls(doc, width, depth, level1, level2);
+            Transaction transaction = new Transaction(doc, "Создание модели");
+            transaction.Start();
+                CreateWalls(doc, width, depth, level1, level2);
+
+                AddDoor(doc, level1, walls[0]);
+                AddWindow(doc, level1, walls[1]);
+                AddWindow(doc, level1, walls[2]);
+                AddWindow(doc, level1, walls[3]);
+            transaction.Commit();
 
             return Result.Succeeded;
+        }
+
+        private void AddWindow(Document doc, Level level1, Wall wall)
+        {
+            FamilySymbol windowType = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_Windows)
+                .OfType<FamilySymbol>()
+                .Where(x => x.Name.Equals("0406 x 0610 мм"))
+                .Where(x => x.FamilyName.Equals("Фиксированные"))
+                .FirstOrDefault();
+
+            LocationCurve hostCurve = wall.Location as LocationCurve;
+            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
+            XYZ point2 = hostCurve.Curve.GetEndPoint(1);
+            XYZ point = (point1 + point2) / 2;
+
+            if (!windowType.IsActive)
+                windowType.Activate();
+
+            doc.Create.NewFamilyInstance(point, windowType, wall, level1, StructuralType.NonStructural)
+                .get_Parameter(BuiltInParameter.INSTANCE_SILL_HEIGHT_PARAM)
+                .Set(UnitUtils.ConvertToInternalUnits(1000, UnitTypeId.Millimeters)); ;
+
+        }
+
+        private void AddDoor(Document doc, Level level1, Wall wall)
+        {
+            FamilySymbol doorType = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_Doors)
+                .OfType<FamilySymbol>()
+                .Where(x=>x.Name.Equals("0915 x 2134 мм"))
+                .Where(x => x.FamilyName.Equals("Одиночные-Щитовые"))
+                .FirstOrDefault();
+
+            LocationCurve hostCurve = wall.Location as LocationCurve;
+            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
+            XYZ point2 = hostCurve.Curve.GetEndPoint(1);
+            XYZ point = (point1+ point2)/2;
+
+            if (!doorType.IsActive)
+                doorType.Activate();
+
+            doc.Create.NewFamilyInstance(point, doorType, wall, level1, StructuralType.NonStructural);
         }
 
         public void CreateWalls(Document doc, double width, double depth, Level levelBot, Level levelTop)
@@ -49,10 +105,6 @@ namespace CreationModelPlugin
             points.Add(new XYZ(-dx, dy, 0));
             points.Add(new XYZ(-dx, -dy, 0));
 
-            List<Wall> walls = new List<Wall>();
-
-            Transaction transaction = new Transaction(doc, "Построение стен");
-            transaction.Start();
             for (int i = 0; i < 4; i++)
             {
                 Line line = Line.CreateBound(points[i], points[i + 1]);
@@ -60,7 +112,6 @@ namespace CreationModelPlugin
                 walls.Add(wall);
                 wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(levelTop.Id);
             }
-            transaction.Commit();
         }
     }
 }
